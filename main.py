fa66cfa8504e7f630a545c5ece5ffd8029b4c734
Cgamess <153407105+Cgamess@users.noreply.github.com>
2024-02-29 21:29:29 -0700

    added code

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pickle as pkl
-import dill
+import dill, json, os
+# import hashlib
+import pickletools as pklt
+from hashlib import *
 import cryptography as cry
+def encode(*input_data):
+    output=b""
+    # code
+    output+=sha3_512(output)+sha3_512(input_data)
+    return output
+def decode(input_data_serialized_vfid):
+    output=""
+    input_data_serialized=input_data_serialized_vfid[:-128]
+    hashes=[input_data_serialized_vfid[-128:-64],input_data_serialized_vfid[-64:]]
+    if hashes[0] != sha3_512(input_data_serialized):
+        raise ValueError("Incorrect first hash")
+    # code
+    if hashes[1] != sha3_512(output):
+        raise ValueError("Incorrect second hash")
