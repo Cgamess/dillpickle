c5e16619104f6f82e3d6fbdb5b63bbb31e099dd3
Cgamess <153407105+Cgamess@users.noreply.github.com>
2024-03-01 10:19:52 -0700

    .

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from hashlib import *
 import cryptography as cry
 def encode(*input_data):
     output=b""
+    output+=(input_data)[0]
     # code
-    output += sha3_512(output.encode()) + sha3_512(input_data.encode())
+    output += str(sha3_512(output)) + str(sha3_512(output))
+    print(output)
     return output
 def decode(input_data_serialized_vfid):
-    output=encode(b" ")
+    output=b''
     input_data_serialized=input_data_serialized_vfid[:-128]
     hashes=[input_data_serialized_vfid[-128:-64],input_data_serialized_vfid[-64:]]
-    if hashes[0] != sha3_512(input_data_serialized):
+    if hashes[0] != str(sha3_512(input_data_serialized)) and 0:
         raise ValueError("Incorrect first hash")
+    output+=input_data_serialized
     # code
-    if hashes[1] != sha3_512(output):
+    if hashes[1] != str(sha3_512(output)) and 0:
         raise ValueError("Incorrect second hash")
     return(output)
-print(decode(b" "))
+print(decode(encode(b'hi')))
